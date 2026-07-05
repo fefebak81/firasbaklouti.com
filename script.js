e2e27163ad2e869e0bd5d5c2b80834bc535df39a
fefebak81 <firas.baklouti@gmail.com>
2026-07-05 20:51:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,16 @@ navbar.style.boxShadow="none";
 }
 
 });
+
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 40) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
