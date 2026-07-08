a1319149eb8ce17ae81788921cff725a1e3c9001
fefebak81 <firas.baklouti@gmail.com>
2026-07-08 19:36:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,11 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
+
     navLinks.classList.toggle("active");
+
+    menuToggle.classList.toggle("open");
+
 });
 
 // Close menu when a link is clicked
